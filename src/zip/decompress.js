4aa173b625630d51f8b7f3eba7fb1548ea3d682e
Ales Drobysh <drobysh.ales@gmail.com>
2025-04-24 22:06:35 +0200

    feat: solve zip tasks

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,22 @@
+import zlib from "zlib";
+import path from "path";
+import url from "url";
+import fs from "fs";
+
 const decompress = async () => {
-    // Write your code here 
+  const sourcePath = path.join(
+    url.fileURLToPath(import.meta.url),
+    "../files/archive.gz",
+  );
+
+  const targetPath = path.join(
+    url.fileURLToPath(import.meta.url),
+    "../files/fileToCompress.txt",
+  );
+
+  fs.createReadStream(sourcePath)
+    .pipe(zlib.createGunzip())
+    .pipe(fs.createWriteStream(targetPath));
 };
 
 await decompress();
