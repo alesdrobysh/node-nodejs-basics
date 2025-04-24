46239cc34844b3227e00749870f630ba99fd872c
Ales Drobysh <drobysh.ales@gmail.com>
2025-04-24 21:58:45 +0200

    feat: solve streams tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import path from "path";
+import url from "url";
+import fs from "fs";
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.join(
+    url.fileURLToPath(import.meta.url),
+    "../files/fileToRead.txt",
+  );
+
+  const stream = fs.createReadStream(filePath);
+
+  stream.pipe(process.stdout);
+  stream.on("end", console.log); // flushes stdout before exiting the process
 };
 
 await read();
