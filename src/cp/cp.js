fd18725c545ae7c1fc1e8ef16c58aa801c2a0221
Ales Drobysh <drobysh.ales@gmail.com>
2025-04-25 20:20:15 +0200

    feat: solve cp tasks

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,17 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import path from "path";
+import url from "url";
+import child_process from "child_process";
+
+const spawnChildProcess = async (args = []) => {
+  const filePath = path.join(
+    url.fileURLToPath(import.meta.url),
+    "../files/script.js",
+  );
+
+  child_process.spawn("node", [filePath, ...args], {
+    stdio: "inherit",
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["qwe", "rty"]);
