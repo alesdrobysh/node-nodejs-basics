67eba6e27f52a325817c3613a7bb953ce12a5e36
Ales Drobysh <drobysh.ales@gmail.com>
2025-04-25 20:41:17 +0200

    fix: sort wt result by worker creation time

--- src/wt/main.js
+++ src/wt/main.js
@@ -5,33 +5,52 @@ import { Worker } from "worker_threads";
 
 const filePath = path.join(url.fileURLToPath(import.meta.url), "../worker.js");
 
-const runWorkers = () => {
-  const workers = os.cpus().map((_, index) => {
-    return new Promise((resolve, reject) => {
-      const worker = new Worker(filePath, {
-        workerData: 11 + index,
+const createWorkers = async () => {
+  const workers = [];
+
+  await Promise.all(
+    os.cpus().map((_, index) => {
+      return new Promise((resolve, reject) => {
+        const worker = new Worker(filePath, {
+          workerData: 11 + index,
+        });
+
+        worker.on("online", () => {
+          workers.push(worker);
+          resolve();
+        });
       });
+    }),
+  );
 
-      worker.on("message", (data) =>
-        resolve({
-          status: "resolved",
-          data,
-        }),
-      );
-      worker.on("error", () =>
-        reject({
-          status: "rejected",
-          data: null,
-        }),
-      );
-    });
-  });
+  return workers;
+};
 
-  return Promise.all(workers);
+const runWorkers = async (workers) => {
+  return Promise.all(
+    workers.map(
+      (worker) =>
+        new Promise((resolve, reject) => {
+          worker.on("message", (data) =>
+            resolve({
+              status: "resolved",
+              data,
+            }),
+          );
+          worker.on("error", () =>
+            reject({
+              status: "rejected",
+              data: null,
+            }),
+          );
+        }),
+    ),
+  );
 };
 
 const performCalculations = async () => {
-  const result = await runWorkers();
+  const workers = await createWorkers();
+  const result = await runWorkers(workers);
 
   console.log(result);
 };
