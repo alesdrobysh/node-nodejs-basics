ba79f2d90909277bca2f7936ecac6bbad456911b
Ales Drobysh <drobysh.ales@gmail.com>
2025-04-25 19:08:58 +0200

    feat: solve wt tasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,39 @@
+import url from "url";
+import path from "path";
+import os from "os";
+import { Worker } from "worker_threads";
+
+const filePath = path.join(url.fileURLToPath(import.meta.url), "../worker.js");
+
+const runWorkers = () => {
+  const workers = os.cpus().map((_, index) => {
+    return new Promise((resolve, reject) => {
+      const worker = new Worker(filePath, {
+        workerData: 11 + index,
+      });
+
+      worker.on("message", (data) =>
+        resolve({
+          status: "resolved",
+          data,
+        }),
+      );
+      worker.on("error", () =>
+        reject({
+          status: "rejected",
+          data: null,
+        }),
+      );
+    });
+  });
+
+  return Promise.all(workers);
+};
+
 const performCalculations = async () => {
-    // Write your code here
+  const result = await runWorkers();
+
+  console.log(result);
 };
 
 await performCalculations();
